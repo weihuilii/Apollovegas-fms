bdfda39aebcb62071662625376a7126b3ff477a0
Junxiang Huang <wacmkxiaoyi@gmail.com>
2025-03-20 17:26:11 +0800

    update auto test

--- autotest/utils/file.py
+++ autotest/utils/file.py
@@ -4,7 +4,7 @@ file_path = 'autotest/test_repo/test_file1.txt'
 def test():
 
     def _raise_assert_error(key, value):
-        assert file.get(key) == value, f'{key} is not correct'
+        assert file.get(key) == value, f'{key} is not correct: current {file.get(key)}, except {value}'
 
     file = File(file_path)
     _raise_assert_error('file_path', file_path)

d7cf51f28b8cddc36a5375d9a896b554aecde860
Junxiang Huang <wacmkxiaoyi@gmail.com>
2025-03-27 16:00:24 +0800

    update utils::file

--- autotest/utils/file.py
+++ autotest/utils/file.py
@@ -3,12 +3,6 @@ file_path = 'autotest/test_repo/test_file1.txt'
 
 def test():
 
-    def _raise_assert_error(key, value):
-        assert file.get(key) == value, f'{key} is not correct: current {file.get(key)}, except {value}'
-
     file = File(file_path)
-    _raise_assert_error('file_path', file_path)
-    _raise_assert_error('size', 10000)
-    _raise_assert_error('directory', 'autotest/test_repo')
-    _raise_assert_error('filename', 'test_file1.txt')
+    assert file.size == 10000, "Incorrect file size"
     
